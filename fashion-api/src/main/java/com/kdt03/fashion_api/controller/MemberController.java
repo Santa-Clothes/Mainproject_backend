bc8f7f860f69c5404cd9b1c66a19a6c44555e0c9
nx2803 <nx2803@gmail.com>
2026-01-29 11:54:26 +0900

    local login with jwttoken

--- fashion-api/src/main/java/com/kdt03/fashion_api/controller/MemberController.java
+++ fashion-api/src/main/java/com/kdt03/fashion_api/controller/MemberController.java
@@ -1,25 +1,33 @@
 package com.kdt03.fashion_api.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kdt03.fashion_api.domain.dto.MemberLoginDTO;
 import com.kdt03.fashion_api.service.MemberService;
+import com.kdt03.fashion_api.util.JWTUtil;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.kdt03.fashion_api.domain.Member;
 import com.kdt03.fashion_api.domain.dto.MemberSignupDTO;
+import com.kdt03.fashion_api.repository.MemberRepository;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
-
+    private final MemberRepository memberRepo;
+    private final JWTUtil jwtUtil;
+    
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody MemberSignupDTO dto) {
         // TODO: process POST request
@@ -30,14 +38,24 @@ public class MemberController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody MemberLoginDTO dto) {
-       
         try {
-            String message = memberService.login(dto);
+            memberService.login(dto);
+            String token = jwtUtil.getJWT(dto.getId());
+            
 
-            return ResponseEntity.ok(message);
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", true);
+            response.put("accessToken", token);
+            response.put("userId", dto.getId());
+            return ResponseEntity.ok(response);
 
         } catch (RuntimeException e) {
-            return ResponseEntity.status(401).body(e.getMessage());
+
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("success", false);
+            errorResponse.put("message", e.getMessage());
+
+            return ResponseEntity.status(401).body(errorResponse);
         }
     }
 
